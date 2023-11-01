04c25025c244d7fafa24001f2b0480c440500d18
Carla Araujo <carla.araujo@ilog.com.br>
2023-11-01 12:22:13 -0300

    Atualização exer388

--- 1semestre/Algoritmos/05Exer0111/Exer388.py
+++ 1semestre/Algoritmos/05Exer0111/Exer388.py
@@ -3,12 +3,16 @@ Na entrada de dados, o número já deverá ser armazenado na sua posição
 definitiva em ordem decrescente.
 Imprimir o vetor logo após a entrada de dados. """
 
-lista = [34,25,70,89]
+lista = []
 
-""" num = int(input('\nDigite um número: '))
+cont = 0
 
-lista.append(num) """
- 
-aaa = lista.sort(key=int, reverse=True) 
+while len(lista)<=9:
 
-print(aaa)
+    cont = cont+1
+
+    num = int(input(f'\nDigite um número o {cont}º: '))
+
+    lista.append(num)
+    
+    print(sorted(lista, reverse=True))
