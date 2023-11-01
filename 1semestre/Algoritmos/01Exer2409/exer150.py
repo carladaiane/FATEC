e100c3b2f162ac18d07226fae72251a961b4bb7a
Carla Araujo <96966614+carladaiane@users.noreply.github.com>
2023-11-01 13:07:04 -0300

    Update exer150.py

--- 1semestre/Algoritmos/01Exer2409/exer150.py
+++ 1semestre/Algoritmos/01Exer2409/exer150.py
@@ -2,16 +2,11 @@ import math as m
 
 ang = float(input('Digite o ângulo em graus: ').replace(",","."))
 
-rang = ang * math.pi / 180
+rang = ang * m.pi / 180
 
-if ((rang > math.pi / 2 and rang <= math.pi) or (rang > 3 * math.pi / 2 and rang <= 2 * math.pi)):
+if ((rang > m.pi / 2 and rang <= m.pi) or (rang > 3 * m.pi / 2 and rang <= 2 * m.pi)):
     print(f'\nseno: {m.sin(rang):.2f}')
 
 else:
     print(f'\nco-seno: {m.cos(rang):.2f}')
 print (f'\n ') 
-
-
-
-
-
