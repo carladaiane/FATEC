2dfa734e13e84e731f71035b099fb0b25010f9da
Carla Araujo <96966614+carladaiane@users.noreply.github.com>
2024-02-28 20:34:34 -0300

    Update exer01.java

--- 2semestre/linguagemProgramacao1/exer27022024/exer01.java
+++ 2semestre/linguagemProgramacao1/exer27022024/exer01.java
@@ -1,1 +1,9 @@
+public class MyFirstJavaProgram {
 
+   public static void main(String []args) {
+      System.out.println("Meu primeiro exercicio com Java"); // prints Hello, World!
+      System.out.println("Agora que o filho chora \n e a mae ve, mas nao entende porque tanta lagrima");
+      
+      
+   }
+}
