f5bcbb58802908af37b9320b6d7b9e30798d2b7f
Carla Araujo <96966614+carladaiane@users.noreply.github.com>
2024-02-28 13:51:26 -0300

    Update exer02-2702.java

--- 2semestre/linguagemProgramacao1/exer02-2702.java
+++ 2semestre/linguagemProgramacao1/exer02-2702.java
@@ -1,1 +1,15 @@
+public class CalculadoraDespesasTrimestre {
+    public static void main(String[] args) {
+        int janeiro = 15000;
+        int fevereiro = 23000;
+        int marco = 17000;
 
+        int total = janeiro + fevereiro + marco;
+        
+        double media = total / 3.0;
+
+        // Imprimindo os resultados
+        System.out.println("Despesa total no trimestre: R$" + total);
+        System.out.println("Média mensal de gastos: R$" + media);
+    }
+}
