7fe84262b2b85a9beb3fdf9fe3edb0162ec436cc
Carla Araujo <96966614+carladaiane@users.noreply.github.com>
2024-02-28 19:21:23 -0300

    Update exer03-2702.java

--- 2semestre/linguagemProgramacao1/exer03-2702.java
+++ 2semestre/linguagemProgramacao1/exer03-2702.java
@@ -24,6 +24,21 @@ public class CalcularNotaLP {
                       (Math.max(((P1 * 0.6 + ((E1 + E2) / 2) * 0.4) - 5.9), 0) /
                       ((P1 * 0.6 + ((E1 + E2) / 2) * 0.4) - 5.9)) * (API * 0.5) + X + (SUB * 0.2);*/
 
+
+
+        // Exemplo para tentiva de condicional
+
+        if (estoque >= 100) {
+	           System.out.println(“Produto com quantidade suficiente.”);
+        } else if (estoque < 100 && estoque > 50) {
+	        System.out.println(“Alerta: Avaliar possibilidade de novo pedido.”);
+        } else {
+	        System.out.println(“ATENÇÃO! Faça um novo pedido.”);
+        }
+
+        
+        
+
       // Usar a fórumla da LP pra grar o calculo
 
         // Exibindo a nota calculada
